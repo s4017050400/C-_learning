e5ff30a734d863674bd7e91b20efeab8786a33b5
s4017050400 <s4017050400@gmail.com>
2020-03-11 19:34:46 +0800

    Create Data_type.cpp

---
+++ contents/Data_type.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase = "Giraffe Academy";
+    int age = 50;
+    //float 
+    double gpa = 2.4;
+    bool isMale = false;
+
+    cout << "grade" << endl;
+
+
+
+
+    return 0;
+}
+
